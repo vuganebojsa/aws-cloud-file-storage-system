b3417b7cde6056f1267f42e2b77cc7af5ed933c7
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-30 15:10:38 +0200

    Added get folders and post folder lambdas

---
+++ awsCloudFileStorageSystem/lambdas/postFolder.py
@@ -0,0 +1,39 @@
+import json
+from .responses import *
+import boto3
+import base64
+import uuid
+
+def post_folder_dynamo(event, context):
+
+    dynamodb_client = boto3.client('dynamodb')
+    info = event['body']
+    info_dict = json.loads(info)
+    item_id = str(uuid.uuid4())
+    try:
+        response = dynamodb_client.put_item(
+            TableName='folder-bivuja-table',
+            Item={
+                'id':{
+                    'S': item_id
+                },
+                'foldername':{
+                    'S': info_dict['foldername']
+                },
+                'username':{
+                    'S': info_dict['username']
+                },
+                'path':{
+                    'S': info_dict['path']
+                }
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': 'Data uploaded successfully.'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Failed to upload data: {str(e)}'
+        }
