42270baa190a260af07050a7298e6a448e8398dc
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-06-12 19:04:13 +0200

    Syntax error fixes

--- awsCloudFileStorageSystem/lambdas/postFileDynamo.py
+++ awsCloudFileStorageSystem/lambdas/postFileDynamo.py
@@ -10,8 +10,9 @@ def post_file_dynamo(event, context):
     info = event['body']
     info_dict = json.loads(info)
     item_id = str(uuid.uuid4())
+    dynamodb = boto3.resource('dynamodb')
 
-    table = dynamodb_client.Table('bivuja-table')
+    table = dynamodb.Table('bivuja-table')
     response = table.scan(
         FilterExpression='filename = :filename and folderName = :folderName and bucketName = :bucketName AND #usr = :giver',
         ExpressionAttributeValues={
@@ -26,9 +27,10 @@ def post_file_dynamo(event, context):
     items = response['Items']
     if len(items) != 0:
         try:
+            print('Usao')
             response = table.update_item(
-            Key={'id': info_dict['id']},
-            UpdateExpression='SET filename = :value1, description = :value2, tags = :value3, type = :value4, size = :value5, lastModifiedAt= :value6, bucketName = :value7, folderName = :value8',
+            Key={'id': items[0]['id']},
+            UpdateExpression='SET filename = :value1, description = :value2, tags = :value3, #tp = :value4, size = :value5, lastModifiedAt= :value6, bucketName = :value7, folderName = :value8',
             ExpressionAttributeValues={
                 ':value1': info_dict['filename'],
                 ':value2': info_dict['description'],
@@ -39,14 +41,19 @@ def post_file_dynamo(event, context):
                 ':value7': info_dict['bucketName'],
                 ':value8': info_dict['folderName'],
             },
+            ExpressionAttributeNames={
+                '#tp': 'type'
+            }, 
             ReturnValues='ALL_NEW')
             return {
                 'statusCode': 200,
                 'body': 'Data edited successfully.'
             }
         except Exception as e:
+            print(e)
+
             return {
-                'statusCode': 401,
+                'statusCode': 400,
                 'body': f'Failed to edit data: {str(e)}'
             }
         
@@ -88,6 +95,6 @@ def post_file_dynamo(event, context):
             }
         except Exception as e:
             return {
-                'statusCode': 500,
+                'statusCode': 400,
                 'body': f'Failed to upload data: {str(e)}'
             }
